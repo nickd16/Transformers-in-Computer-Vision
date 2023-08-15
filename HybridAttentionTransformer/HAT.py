c6e4fbbd18dcf7f94f03ffc56aef0490204aafea
nickd16 <nickd16718@gmail.com>
2023-08-15 19:24:18 -0400

    finished OCA and RHAG

--- HybridAttentionTransformer/HAT.py
+++ HybridAttentionTransformer/HAT.py
@@ -11,6 +11,29 @@ import time
 
 # SWMSA Input -> (B, C, H, W)
 
+class RelativeSinusoidalEmbeddings(nn.Module):
+    def __init__(self, window_size, overlap=0):
+        super().__init__()
+        expanded_window = int(window_size * (overlap+1))
+        C = 2*window_size-1
+        D = 2*expanded_window-1
+        B = torch.zeros(C, D)
+        positions = torch.arange(C).unsqueeze(1)
+        divisor = torch.exp(torch.arange(0, D, 2) * (-math.log(10000.0) / D))
+        angles = positions * divisor
+        B[:, 0::2] = torch.sin(angles)
+        B[:, 1::2] = torch.cos(angles) if (B.shape[0] % 2 == 0 and B.shape[1] % 2 == 0) else torch.cos(angles[:,:-1])
+        xrows = torch.arange(1,window_size+1,1/window_size)
+        xcols = torch.arange(1,expanded_window+1,1/expanded_window)
+        x = (xcols[None, :]-xrows[:, None]).int()
+        yrows = torch.concat([torch.arange(1,window_size+1)] * window_size)
+        ycols = torch.concat([torch.arange(1,expanded_window+1)] * expanded_window)
+        y = (ycols[None, :]-yrows[:, None])
+        self.embeddings = nn.Parameter((B[x[:,:], y[:,:]]), requires_grad=False)
+
+    def forward(self, x):
+        return x + self.embeddings
+    
 class ShiftedWindowMSA(nn.Module):
     def __init__(self, embed_dim, num_heads, window_size, mask=False):
         super().__init__()
@@ -20,11 +43,11 @@ class ShiftedWindowMSA(nn.Module):
         self.mask = mask
         self.proj1 = nn.Linear(embed_dim, 3*embed_dim)
         self.proj2 = nn.Linear(embed_dim, embed_dim)
-        self.embeddings = RelativeSinusoidalEmbeddings()
+        self.embeddings = RelativeSinusoidalEmbeddings(window_size)
 
     def forward(self, x):
         h_dim = self.embed_dim / self.num_heads
-        height, width = x.shape[1:3]
+        height, width = x.shape[2:4]
         x = rearrange(x, 'b c h w -> b (h w) c')
         x = self.proj1(x)
         x = rearrange(x, 'b (h w) (c K) -> b h w c K', K=3, h=height, w=width)
@@ -58,25 +81,6 @@ class ShiftedWindowMSA(nn.Module):
         x = self.proj2(x)
         x = rearrange(x, 'b (h w) c -> b c h w', h=height, w=width)
         return x
-    
-class RelativeSinusoidalEmbeddings(nn.Module):
-    def __init__(self, window_size):
-        super().__init__()
-        C = 2*window_size-1
-        B = torch.zeros(C, C)
-        positions = torch.arange(C).unsqueeze(1)
-        divisor = torch.exp(torch.arange(0, C, 2) * (-math.log(10000.0) / C))
-        angles = positions * divisor
-        B[:, 0::2] = torch.sin(angles)
-        B[:, 1::2] = torch.cos(angles[:,:-1])
-        x = torch.arange(1,window_size+1,1/window_size)
-        x = (x[None, :]-x[:, None]).int()
-        y = torch.concat([torch.arange(1,window_size+1)] * window_size)
-        y = (y[None, :]-y[:, None])
-        self.embeddings = nn.Parameter((B[x[:,:], y[:,:]]), requires_grad=False)
-
-    def forward(self, x):
-        return x + self.embeddings
     
 class RelativeLearnedEmbeddings(nn.Module):
     def __init__(self, window_size=7):
@@ -128,15 +132,17 @@ class HybridAttentionBlock(nn.Module):
 
     def forward(self, x):
         height, width = x.shape[2:]
-        norm = self.layer_norm(x)
+        norm = self.layer_norm(x.permute(0,2,3,1)).permute(0,3,1,2)
         norm = (self.alpha * self.CAB(norm)) + self.WMSA(norm)
         x = self.dropout(x + norm)
-        norm = self.layer_norm(x)
+        norm = self.layer_norm(x.permute(0,2,3,1)).permute(0,3,1,2)
         norm = rearrange(norm, 'B C H W -> B (H W) C')
         norm = self.MLP(norm)
         norm = rearrange(norm, 'B (H W) C -> B C H W', H=height, W=width)
         return self.dropout(x + norm)
-  
+
+# HAB Block Input -> (B, C, H, W)
+
 class OverlappingCrossAttention(nn.Module):
     def __init__(self, embed_dim, num_heads, window_size, overlap_size):
         super().__init__()
@@ -146,7 +152,7 @@ class OverlappingCrossAttention(nn.Module):
         self.lambd = overlap_size
         self.proj1 = nn.Linear(embed_dim, 3*embed_dim)
         self.proj2 = nn.Linear(embed_dim, embed_dim)
-        self.embeddings = RelativeSinusoidalEmbeddings()
+        self.embeddings = RelativeSinusoidalEmbeddings(window_size, overlap_size)
         padding = int((overlap_size*window_size)/2)
         self.Mo = int((1 + overlap_size) * window_size)
         self.unfold = nn.Unfold(kernel_size=self.Mo, stride=window_size, padding=padding)
@@ -167,28 +173,71 @@ class OverlappingCrossAttention(nn.Module):
         V = rearrange(V, 'B (H c s1 s2) (h w) -> B H h w (s1 s2) c', H=self.num_heads, s1=self.Mo, s2=self.Mo, h=num_windows, w=num_windows)
         
         att_scores = (Q @ K.transpose(4,5)) / math.sqrt(h_dim)
+        att_scores = self.embeddings(att_scores)
+        att = F.softmax(att_scores, dim=-1) @ V
+        x = rearrange(att, 'b H h w (m1 m2) E -> b (h m1) (w m2) (H E)', m1=self.window_size, m2=self.window_size)
+        x = rearrange(x, 'b h w c -> b (h w) c')
+        x = self.proj2(x)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=height, w=width)
 
         return x
     
 class CrossAttentionBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_dim, window_size, num_heads, dropout=0.1, overlap_size=0.5):
         super().__init__()
+        self.layer_norm = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.OCA = OverlappingCrossAttention(embed_dim, num_heads, window_size, overlap_size)
+        self.MLP = nn.Sequential(
+            nn.Linear(embed_dim, embed_dim*2),
+            nn.GELU(),
+            nn.Linear(embed_dim*2, embed_dim)
+        )
 
     def forward(self, x):
+        height, width = x.shape[2:]
+        norm = self.layer_norm(x.permute(0,2,3,1)).permute(0,3,1,2)
+        norm = self.OCA(norm)
+        x = self.dropout(x + norm)
+        norm = self.layer_norm(x.permute(0,2,3,1)).permute(0,3,1,2)
+        norm = rearrange(norm, 'B C H W -> B (H W) C')
+        norm = self.MLP(norm)
+        norm = rearrange(norm, 'B (H W) C -> B C H W', H=height, W=width)
+        return self.dropout(x + norm)
+
+class ResidualHybridAttentionGroup(nn.Module):
+    def __init__(self, embed_dim=30, window_size=16, num_heads=6, dropout=0.1, squeeze_factor=3, overlap_size=0.5, alpha=0.01):
+        super().__init__()
+        self.HAB1 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout)
+        self.HAB2 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout, shift=True)
+        self.HAB3 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout)
+        self.HAB4 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout, shift=True)
+        self.HAB5 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout)
+        self.HAB6 = HybridAttentionBlock(embed_dim, window_size, num_heads, squeeze_factor, alpha, dropout, shift=True)
+        self.OCA = OverlappingCrossAttention(embed_dim, num_heads, window_size, overlap_size)
+
+    def forward(self, x):
+        for i in range(1,7):
+            layer = getattr(self, f'HAB{i}')
+            x = layer(x)
+        x = self.OCA(x)
         return x
 
-class ResidualAttentionGroup(nn.Module):
-    def __init__(self):
+class HAT_model(nn.Module):
+    def __init__(self, C=30):
         super().__init__()
+        self.conv1 = nn.Conv2d(3, C, padding=1)
+        self.conv2 = nn.Conv2d(C, C, padding=1)
+        self.RHAG = ResidualHybridAttentionGroup()
 
     def forward(self, x):
+        y = self.conv1(x)
+        x = self.RHAG(y)
+        x = self.conv2(x) + y
         return x
 
 def main():
-    x = torch.zeros((64, 30, 224, 224))
-    A = OverlappingCrossAttention(30, 6, 16, .5)
-    x = A(x)
-    # print(x.shape)
+    x = torch.zeros((1, 30, 224, 224)).cuda()
 
 if __name__ == '__main__':
     main()
--- HybridAttentionTransformer/test.py
+++ HybridAttentionTransformer/test.py
@@ -9,16 +9,22 @@ from sklearn.decomposition import PCA
 import math
 import time
 
-
+window_size = 16
+overlap=.5
+expanded_window = int(window_size * (overlap+1))
 C = 2*window_size-1
-B = torch.zeros(C, C)
+D = 2*expanded_window-1
+B = torch.zeros(C, D)
 positions = torch.arange(C).unsqueeze(1)
-divisor = torch.exp(torch.arange(0, C, 2) * (-math.log(10000.0) / C))
+divisor = torch.exp(torch.arange(0, D, 2) * (-math.log(10000.0) / D))
 angles = positions * divisor
 B[:, 0::2] = torch.sin(angles)
-B[:, 1::2] = torch.cos(angles[:,:-1])
-x = torch.arange(1,window_size+1,1/window_size)
-x = (x[None, :]-x[:, None]).int()
-y = torch.concat([torch.arange(1,window_size+1)] * window_size)
-y = (y[None, :]-y[:, None])
-self.embeddings = nn.Parameter((B[x[:,:], y[:,:]]), requires_grad=False)
+B[:, 1::2] = torch.cos(angles) if B.shape[1] % 2 == 0 else torch.cos(angles[:,:-1])
+xrows = torch.arange(1,window_size+1,1/window_size)
+xcols = torch.arange(1,expanded_window+1,1/expanded_window)
+x = (xcols[None, :]-xrows[:, None]).int()
+yrows = torch.concat([torch.arange(1,window_size+1)] * window_size)
+ycols = torch.concat([torch.arange(1,expanded_window+1)] * expanded_window)
+y = (ycols[None, :]-yrows[:, None])
+embeddings = nn.Parameter((B[x[:,:], y[:,:]]), requires_grad=False)
+print(embeddings.shape)
